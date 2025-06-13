7606de3b786fb185b6fcffa8bbdef8d742737709
Wahyu Nur Salam <wahyurhythm@gmail.com>
2025-06-13 19:26:14 +0700

    add product image

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -18,14 +18,16 @@ class ProductController extends BaseController
                 'discount' => 0,
                 'discount_type' => 'none',
                 'discount_price' => 3500,
+                'image' => 'https://down-id.img.susercontent.com/file/6639c3e16e12392af7c0a87448c7390f'
             ],
             [
                 'product_code' => 'PRD002',
-                'product_name' => 'Kopi ABC Susu 31g',
+                'product_name' => 'Kopi ABC Susu',
                 'price' => 2500,
                 'discount' => 10,
                 'discount_type' => 'percent',
                 'discount_price' => 2250,
+                'image' => 'https://down-id.img.susercontent.com/file/id-11134207-7qula-lh2unj8i2a2a1d'
             ],
             [
                 'product_code' => 'PRD003',
@@ -34,6 +36,7 @@ class ProductController extends BaseController
                 'discount' => 500,
                 'discount_type' => 'fixed',
                 'discount_price' => 5500,
+                'image' => 'https://down-id.img.susercontent.com/file/6dd095a73524af106db989c727d6ad86'
             ],
             [
                 'product_code' => 'PRD004',
@@ -42,38 +45,43 @@ class ProductController extends BaseController
                 'discount' => 0,
                 'discount_type' => 'none',
                 'discount_price' => 4000,
+                'image' => 'https://down-id.img.susercontent.com/file/9772c3b4ee30c1c5110eb1ad0ee401af'
             ],
             [
                 'product_code' => 'PRD005',
-                'product_name' => 'SilverQueen Chunky Bar 62g',
+                'product_name' => 'SilverQueen Chunky Bar',
                 'price' => 12500,
                 'discount' => 5,
                 'discount_type' => 'percent',
                 'discount_price' => 11875,
+                'image' => 'https://down-id.img.susercontent.com/file/id-11134207-7r98o-lm08keh2mg81cb'
             ],
             [
                 'product_code' => 'PRD006',
-                'product_name' => 'Teh Pucuk Harum 350ml',
+                'product_name' => 'Teh Pucuk Harum',
                 'price' => 5000,
                 'discount' => 1000,
                 'discount_type' => 'fixed',
                 'discount_price' => 4000,
+                'image' => 'https://down-id.img.susercontent.com/file/4fb9f965231c5c6f4bd8516eb9470b92'
             ],
             [
                 'product_code' => 'PRD007',
-                'product_name' => 'Chitato Sapi Panggang 68g',
+                'product_name' => 'Chitato Sapi Panggang',
                 'price' => 8500,
                 'discount' => 0,
                 'discount_type' => 'none',
                 'discount_price' => 8500,
+                'image' => 'https://down-id.img.susercontent.com/file/13c32f2ce2f54fd918c094a99b3e2fb9'
             ],
             [
                 'product_code' => 'PRD008',
-                'product_name' => 'Roti Sari Roti Tawar 200g',
+                'product_name' => 'Roti Sari Roti Tawar',
                 'price' => 12000,
                 'discount' => 2000,
                 'discount_type' => 'fixed',
                 'discount_price' => 10000,
+                'image' => 'https://down-id.img.susercontent.com/file/id-11134201-23030-i5uhewikpfov26'
             ],
             [
                 'product_code' => 'PRD009',
@@ -82,14 +90,16 @@ class ProductController extends BaseController
                 'discount' => 1500,
                 'discount_type' => 'fixed',
                 'discount_price' => 17000,
+                'image' => 'https://down-id.img.susercontent.com/file/b5bb900865b9b9fb8fbc08a8eaa0e3aa'
             ],
             [
                 'product_code' => 'PRD010',
-                'product_name' => 'Sabun Lifebuoy Merah 85g',
+                'product_name' => 'Sabun Lifebuoy Merah',
                 'price' => 3500,
                 'discount' => 10,
                 'discount_type' => 'percent',
                 'discount_price' => 3150,
+                'image' => 'https://down-id.img.susercontent.com/file/226cbac6cc04d90b876d8000a8cafe39'
             ],
         ]);
 
